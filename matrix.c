d87620b2a12201dfe54b18a4998e71b4f6e7001a
PCov3r <pcov3r@gmail.com>
2025-10-14 11:38:03 +0200

    Seesaw init

--- matrix.c
+++ matrix.c
@@ -3,10 +3,15 @@
 #include "print.h"
 #include "seesaw.h"
 
+void SeeSaw_Init(void);
+void resetAllRegisters(void);
+bool SeeSaw_Write(uint8_t regHigh, uint8_t regLow, const uint8_t *buf, uint8_t num);
+
 // QMK required function: Initialize the matrix pins
 void matrix_init_custom(void) {
     // Set row pins as outputs
-    i2c_init();
+    SeeSaw_Init();
+    
     
 }
 
@@ -15,4 +20,40 @@ bool matrix_scan_custom(matrix_row_t current_matrix[]) {
     i2c_status_t status = i2c_ping_address(MY_I2C_ADDRESS, 100);
     uprintf("%d",status);
     return false;
+}
+
+
+void SeeSaw_Init(void)
+{
+
+    i2c_init();
+    resetAllRegisters();
+}
+
+void resetAllRegisters(void)
+{
+    uint8_t val = STATUS_RESET_REG;
+    SeeSaw_Write(STATUS_BASE_REG, STATUS_SWRST_REG, &val, 1);
+}
+
+bool SeeSaw_Write(uint8_t regHigh, uint8_t regLow, const uint8_t *buf, uint8_t num)
+{
+    uint8_t data[2 + num];
+
+    data[0] = regHigh;
+    data[1] = regLow;
+
+    // Copy payload if provided
+    if (buf != NULL && num > 0)
+    {
+        memcpy(&data[2], buf, num);
+    }
+
+    if (!i2c_transmit(MY_I2C_ADDRESS, data, 2 + num, 50))
+    {
+        return false;
+    }
+
+    return true;
+
 }
--- seesaw.h
+++ seesaw.h
@@ -1,3 +1,6 @@
 #include "i2c_master.h"
 
 #define MY_I2C_ADDRESS (0x2E << 1)
+#define STATUS_BASE_REG 0x00
+#define STATUS_RESET_REG 0xff
+#define STATUS_SWRST_REG 0x7f
